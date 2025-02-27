3533e2a27a37d5f230806e0ed5b7b15fa4a175c6
SarvT <91484197+SarvT@users.noreply.github.com>
2025-02-27 11:48:20 +0530

    finished branch 3

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,9 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import express from "express";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const router = express.Router();
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -19,17 +22,16 @@ router.post("/login", async (req, res) => {
   res.json({ token });
 });
 
-
-router.post('/register', async (req, res) => {
-    const { username, password, role } = req.body;
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    const newUser = new User({ username, password: hashedPassword, role });
-    await newUser.save();
-    res.status(201).json({ message: 'User registered successfully' });
-  });
+router.post("/register", async (req, res) => {
+  const { username, password, role } = req.body;
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, password: hashedPassword, role });
+  await newUser.save();
+  res.status(201).json({ message: "User registered successfully" });
+});
 
 export { router };
